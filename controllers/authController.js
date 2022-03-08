845ab74c6c6578280a23c2f11ffa68ca00402742
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2022-03-08 19:39:27 +0530

    Forget password

--- controllers/authController.js
+++ controllers/authController.js
@@ -192,7 +192,7 @@ exports.forgotPassword = async(req, res, next) => {
     // 3) Send it to user's email
     const resetURL = `${req.protocol}://${req.get("host")}/api/users/resetPassword/${resetToken}`
 
-    const message = `Forgot your password? Click on the link ${resetURL} and reset your password by entering your new password and this code : ${resetToken}.\nIf you didn't forget your password, please ignore this email!`
+    const message = `Forgot your password? Reset your password by entering your new password and this code : ${resetToken}\nIf you didn't forget your password, please ignore this email!`
 
     try {
         await sendEmail({
